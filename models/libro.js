673a36e4ef062254ecf0b11e7b5b01c5c8298a7f
PabloIMG <pablo16mg@gmail.com>
2021-11-09 23:58:43 -0300

    Modelo libro

---
+++ models/libro.js
@@ -0,0 +1,59 @@
+'use strict'
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema // Relación entre BD y objeto que se creará en el código.
+
+// Schema personalizado.
+const LibroSchema = Schema(
+
+    /*
+        VARIABLES
+
+        nombre: string
+        anio: number
+        idioma: string enum[‘ING’,’ESP’]
+        autor : string
+
+    */
+
+    {
+        nombre:
+        {
+            type:String,
+            required: true,
+            validate:
+            {
+                validator: function (v)
+                {
+                    let isValid = false;
+                    const pattern = new RegExp('^[A-ZÁÉÍÓÚÑ ]+$', 'i');
+                    if (pattern.test(v))
+                    {
+                        isValid = true;
+                    }
+                    return isValid;
+                },
+                message: props => `nombre no tiene formato correcto`
+            }
+        },
+
+        anio:
+        {
+            type: Number
+        },
+
+        idioma:
+        {
+            type: String,
+            enum: ['ING','ESP'],
+            required: true
+        },
+
+        autor:
+        {
+            type: String,
+            required: true
+        }
+
+    })
+
+module.exports = mongoose.model('libros', LibroSchema)
